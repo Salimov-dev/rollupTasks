3ab8ea57ff7888d8b50a0557628bf344dce87c87
Ruslan Salimov <work@salimov-dev.ru>
2022-10-17 21:05:29 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import "./index.css";
+import JS_IMAGE from './assets/Rollup.jpg';
+
 console.log('Hello World!');
+
+const jsImageHTML = document.createElement('img')
+jsImageHTML.className = 'js-image'
+jsImageHTML.src = JS_IMAGE
+
+document.body.append(jsImageHTML)
